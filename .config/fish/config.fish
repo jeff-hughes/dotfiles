1917b57b14703ffc56503a4e12a5f612f44d443e
Jeff Hughes <jeff.hughes@gmail.com>
2021-12-04 13:08:22 -0500

    Move fish functions into fish aliases

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+#alias ls="command ls -AF --group-directories-first --color=auto"
+#alias ll="command ls -lhAF --group-directories-first --color=auto"
+alias cdup="cd .."
+
+#alias cp="command cp -i"
+#alias mv="command mv -i"
+#alias mkdir="command mkdir -pv"
+
+alias venv="source venv/bin/activate.fish"
+alias tmux-kill="tmux kill-session"
+
+alias pm="pacman"
+
+alias xc="xclip -selection clipboard"
