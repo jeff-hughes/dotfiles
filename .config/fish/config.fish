0b96f8875febf8a8432ea648e737ecaf8c777894
Jeff Hughes <jeff.hughes@gmail.com>
2023-10-30 16:21:51 -0400

    Miscellaneous changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,13 @@ alias vim="nvim"
 alias venv="source venv/bin/activate.fish"
 alias tmux-kill="tmux kill-session"
 
+alias shellcaster="shellcaster -c /automnt/extMedia/.config/shellcaster/config.toml"
+
 alias pm="pacman"
 
 alias xc="xclip -selection clipboard"
 
 alias gitdiff="GIT_EXTERNAL_DIFF=difft git diff"
+
+# for editing mods for Elder Scrolls: Oblivion
+alias wrye="STEAM_COMPAT_DATA_PATH=~/.local/share/Steam/steamapps/compatdata/22330/ ~/.local/share/Steam/steamapps/common/Proton\ 5.13/proton run ~/.local/share/Steam/steamapps/common/Oblivion/Mopy/Wrye\ Bash.exe"
